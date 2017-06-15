e4e572dbce3b200a6a0767a1c1e4a64e364af8f9
xinqch <xinchao32119@163.com>
2017-06-15 17:59:28 +0800

    更新log4j2.xml"

--- rapidbuild-parent/rapidbuild-log/src/main/java/com/rapidbuild/log/RapidbuildLogApplication.java
+++ rapidbuild-parent/rapidbuild-log/src/main/java/com/rapidbuild/log/RapidbuildLogApplication.java
@@ -2,15 +2,14 @@ package com.rapidbuild.log;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class RapidbuildLogApplication {
 
-	private static final Logger logger = LogManager.getLogger(RapidbuildLogApplication.class.getName());
+    private static final Logger logger = LogManager.getLogger(RapidbuildLogApplication.class.getName());
 
-	public static void main(String[] args) {
-		logger.info("12312321321321321321312321321321312312312312312");
-	}
+    public static void main(String[] args) {
+        logger.trace("entity");
+        logger.info("12312321321321321321312321321321312312312312312");
+        logger.trace("exit");
+    }
 }
--- rapidbuild-parent/rapidbuild-log/src/main/java/com/rapidbuild/log/config/LogConfig.java
+++ rapidbuild-parent/rapidbuild-log/src/main/java/com/rapidbuild/log/config/LogConfig.java
@@ -1,30 +1,38 @@
-package com.rapidbuild.log.config;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.Layout;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.FileAppender;
-import org.apache.logging.log4j.core.config.AppenderRef;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.LoggerConfig;
-import org.apache.logging.log4j.core.layout.PatternLayout;
-import org.slf4j.LoggerFactory;
-
-/**
- * Created by Administrator on 2017/6/14.
- */
-public class LogConfig {
-
-    public static void logInit(int jobId){
-        //为false时，返回多个LoggerContext对象，   true：返回唯一的单例LoggerContext
+//package com.rapidbuild.log.config;
+//
+//import org.apache.logging.log4j.Level;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.core.Appender;
+//import org.apache.logging.log4j.core.Layout;
+//import org.apache.logging.log4j.core.LoggerContext;
+//import org.apache.logging.log4j.core.appender.FileAppender;
+//import org.apache.logging.log4j.core.config.AppenderRef;
+//import org.apache.logging.log4j.core.config.Configuration;
+//import org.apache.logging.log4j.core.config.LoggerConfig;
+//import org.apache.logging.log4j.core.layout.PatternLayout;
+//import org.slf4j.LoggerFactory;
+//
+///**
+// * Created by Administrator on 2017/6/14.
+// */
+//public class LogConfig {
+//
+//    public static void logInit(int jobId){
+//        //为false时，返回多个LoggerContext对象，   true：返回唯一的单例LoggerContext
 //        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
 //        final Configuration config = ctx.getConfiguration();
 //
+//
+//        new String("");
+//
+//
+//
+//
 //        //创建一个展示的样式：PatternLayout，   还有其他的日志打印样式。
-//        Layout layout = PatternLayout.createLayout(PatternLayout.DEFAULT_CONVERSION_PATTERN,
-//                config, null, null, true, false, null, null);
+////        Layout layout = PatternLayout.createLayout()
+////                .createLayout(PatternLayout.DEFAULT_CONVERSION_PATTERN,
+////                config, null, null, true, false, null, null);
 //
 //        //  日志打印方式——输出为文件
 //        Appender appender = FileAppender.createAppender(
@@ -40,25 +48,25 @@ public class LogConfig {
 //        loggerConfig.addAppender(appender, null, null);
 //        config.addLogger("" + jobId, loggerConfig);
 //        ctx.updateLoggers();
-    }
-
-    public static void stop(int jobId) {
-        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        final Configuration config = ctx.getConfiguration();
-        config.getAppender("" + jobId).stop();
-        config.getLoggerConfig("" + jobId).removeAppender("" + jobId);
-        config.removeLogger("" + jobId);
-        ctx.updateLoggers();
-    }
-    /**
-     * 获取Logger
-     *
-     * 如果不想使用slf4j,那这里改成直接返回Log4j的Logger即可
-     * @param jobId
-     * @return
-     */
-    public static Logger createLogger(int jobId) {
-        logInit(jobId);
-        return (Logger) LoggerFactory.getLogger("" + jobId);
-    }
-}
+//    }
+//
+//    public static void stop(int jobId) {
+//        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+//        final Configuration config = ctx.getConfiguration();
+//        config.getAppender("" + jobId).stop();
+//        config.getLoggerConfig("" + jobId).removeAppender("" + jobId);
+//        config.removeLogger("" + jobId);
+//        ctx.updateLoggers();
+//    }
+//    /**
+//     * 获取Logger
+//     *
+//     * 如果不想使用slf4j,那这里改成直接返回Log4j的Logger即可
+//     * @param jobId
+//     * @return
+//     */
+//    public static Logger createLogger(int jobId) {
+//        logInit(jobId);
+//        return (Logger) LoggerFactory.getLogger("" + jobId);
+//    }
+//}
